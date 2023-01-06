5fa4499f00d67f8d4a93b8f45a5d2eee3c055cd0
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-01-06 02:04:49 -0600

    added the forms for login and sign up

--- forms.py
+++ forms.py
@@ -1,7 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
-from wtforms.validators import InputRequired
+from wtforms import StringField, PasswordField, EmailField
+from wtforms.validators import InputRequired, Length
 
 
-class AddUserForm(FlaskForm):
+class SignupForm(FlaskForm):
+    first_name = StringField("First Name", validators=[InputRequired()])
+    last_name = StringField("Last Name", validators=[InputRequired()])
+    email = EmailField("Email", validators=[InputRequired()])
     username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("passwrod", validators=[
+                             InputRequired(), Length(min=8, max=20, message=None)])
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("passwrod", validators=[InputRequired()])
