67ae03b0423cdf32f16b5c89a370337be43670c4
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-01-15 15:06:14 -0600

    added the bathroom form, corrected the spelling of password, and added the optinal tag

--- forms.py
+++ forms.py
@@ -1,17 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, EmailField
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, Optional
 
 
 class SignupForm(FlaskForm):
-    first_name = StringField("First Name", validators=[InputRequired()])
-    last_name = StringField("Last Name", validators=[InputRequired()])
+    full_name = StringField("Full Name", validators=[InputRequired()])
     email = EmailField("Email", validators=[InputRequired()])
     username = StringField("Username", validators=[InputRequired()])
-    password = PasswordField("passwrod", validators=[
+    password = PasswordField("Password", validators=[
                              InputRequired(), Length(min=8, max=20, message=None)])
 
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired()])
-    password = PasswordField("passwrod", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+
+
+class BathroomForm(FlaskForm):
+    name = StringField("Location Name", validators=[InputRequired()])
+    address = StringField("Address", validators=[InputRequired()])
+    zip_code = StringField("Zip Code", validators=[InputRequired()])
+    website = StringField("Website", validators=[Optional()])
