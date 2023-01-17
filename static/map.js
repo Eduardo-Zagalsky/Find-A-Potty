9adeffd08709c26e720fffa7b432ccc8d91cd11f
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-01-17 08:32:21 -0600

    added the search logic,incomplete, added the pop ups for the markers, turned all the data received into an array of objects, and attempted to export it to the flask page, but failed

--- static/map.js
+++ static/map.js
@@ -1,9 +1,10 @@
 async function getMapInfo() {
     let response = await axios.get("https://www.arcgis.com/sharing/rest/content/items/cc11a895caa848a886014c75835d2d91/data?f=json");
     let result = response.data.operationalLayers[0]["featureCollection"]["layers"][0]["featureSet"]["features"];
+    let bathrooms = [];
     for (let i = 0; i < result.length; i++) {
         let bathroom = result[i]["attributes"];
-        let bathrooms = {
+        let currBathroom = {
             'name': `${bathroom.building_name} ${bathroom.building_type}`,
             'address': bathroom.address,
             'zip_code': bathroom.zip,
@@ -11,14 +12,20 @@ async function getMapInfo() {
             'longitude': bathroom.longitude,
             'website': bathroom.site_link
         };
+        bathrooms.push(currBathroom);
+        // create the popup
+        const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+            `${currBathroom.name}\n${currBathroom.address}\n${currBathroom.zip_code}`
+        );
         // Set marker options.
         const marker = new mapboxgl.Marker({
             color: "#a83232",
             draggable: false
         }).setLngLat([bathroom.longitude, bathroom.latitude])
+            .setPopup(popup) // sets a popup on this marker
             .addTo(map);
-        await axios.post("/initiate", bathrooms)
     };
+    await axios.post("/initiate", JSON.stringify(bathrooms));
 };
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWR1YXJkbzA3OTYiLCJhIjoiY2xjd3BtcW1zMWJsNDQxcDV3OW1ybGkxbyJ9.MGOMpqBI1QdQu27kFXBVww';
@@ -28,4 +35,11 @@ const map = new mapboxgl.Map({
     center: [-87.6565, 41.9491], // starting position [lng, lat]
     zoom: 10, // starting zoom
 });
-getMapInfo()
+
+let searchButton = document.getElementById("search-button")
+searchButton.addEventListener("click", async function (event) {
+    event.preventDefault();
+    let search = document.getElementById("search").value;
+    let getLocation = await axios.get("https://api.mapbox.com/search/v1/forward/", { params: { q: search } });
+});
+getMapInfo();
