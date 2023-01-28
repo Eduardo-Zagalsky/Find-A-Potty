934449052a394f41c31ddaed2d4022d023de32f1
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-01-27 21:31:49 -0600

    added api for map, marker, popup, and narrowed down results.

--- static/map.js
+++ static/map.js
@@ -1,31 +1,21 @@
-async function getMapInfo() {
+let lng
+let lat
+let bathrooms = [];
+async function getMarkers() {
     let response = await axios.get("https://www.arcgis.com/sharing/rest/content/items/cc11a895caa848a886014c75835d2d91/data?f=json");
     let result = response.data.operationalLayers[0]["featureCollection"]["layers"][0]["featureSet"]["features"];
-    let bathrooms = [];
     for (let i = 0; i < result.length; i++) {
         let bathroom = result[i]["attributes"];
         let currBathroom = {
-            'name': `${bathroom.building_name} ${bathroom.building_type}`,
+            'name': bathroom.building_name,
             'address': bathroom.address,
             'zip_code': bathroom.zip,
-            'latitude': bathroom.latitude,
             'longitude': bathroom.longitude,
+            'latitude': bathroom.latitude,
             'website': bathroom.site_link
         };
         bathrooms.push(currBathroom);
-        // create the popup
-        const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-            `${currBathroom.name}\n${currBathroom.address}\n${currBathroom.zip_code}`
-        );
-        // Set marker options.
-        const marker = new mapboxgl.Marker({
-            color: "#a83232",
-            draggable: false
-        }).setLngLat([bathroom.longitude, bathroom.latitude])
-            .setPopup(popup) // sets a popup on this marker
-            .addTo(map);
     };
-    // await axios.post("/initiate", JSON.stringify(bathrooms));
 };
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWR1YXJkbzA3OTYiLCJhIjoiY2xjd3BtcW1zMWJsNDQxcDV3OW1ybGkxbyJ9.MGOMpqBI1QdQu27kFXBVww';
@@ -33,14 +23,44 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
     center: [-87.6565, 41.9491], // starting position [lng, lat]
-    zoom: 10, // starting zoom
+    zoom: 9, // starting zoom
 });
 
-let searchButton = document.getElementById("search-button")
+let searchButton = document.getElementById("search-button");
 searchButton.addEventListener("click", async function (event) {
     event.preventDefault();
     let search = document.getElementById("search").value;
-    let getLocation = await axios.get(`https://api.mapbox.com/search/v12/forward/${search}`, { params: { access_token: 'pk.eyJ1IjoiZWR1YXJkbzA3OTYiLCJhIjoiY2xjd3BtcW1zMWJsNDQxcDV3OW1ybGkxbyJ9.MGOMpqBI1QdQu27kFXBVww' } });
-    console.log(getLocation);
+    const options = {
+        method: 'GET',
+        url: 'https://google-maps-geocoding.p.rapidapi.com/geocode/json',
+        params: { address: search, language: 'en' },
+        headers: {
+            'X-RapidAPI-Key': 'ad9e5957c1msh43164e3dccfa3d0p1ea1f0jsn6c20c5aeb72b',
+            'X-RapidAPI-Host': 'google-maps-geocoding.p.rapidapi.com'
+        }
+    };
+    axios.request(options).then(function (response) {
+        address = response.data.results[0]["formatted_address"];
+        zipCode = response.data.results[0]["address_components"][7]["long_name"];
+        lng = response.data.results[0]["geometry"]["location"].lng;
+        lat = response.data.results[0]["geometry"]["location"].lat;
+        getMarkers();
+        for (let i = 0; i < bathrooms.length; i++) {
+            if (bathrooms[i]['zip_code'] == zipCode) {
+                // create the popup
+                const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+                    `${bathrooms[i]["address"]}`
+                );
+                // Set marker options.
+                const marker = new mapboxgl.Marker({
+                    color: "#a83232",
+                    draggable: false
+                }).setLngLat([bathrooms[i]['longitude'], bathrooms[i]['latitude']])
+                    .setPopup(popup) // sets a popup on this marker
+                    .addTo(map);
+            };
+        };
+    }).catch(function (error) {
+        console.error(error);
+    });
 });
-getMapInfo();
