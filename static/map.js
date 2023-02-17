f1dca026f95d95e8e94aad990b7a57b851942f88
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-02-17 01:48:29 -0600

    added the logic to make a route...it is not working

--- static/map.js
+++ static/map.js
@@ -33,13 +33,8 @@ async function makeRoute() {
         // make a directions request using cycling profile
         // an arbitrary start will always be the same
         // only the end or destination will change
-        const query = await fetch(
-            `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
-            { method: 'GET' }
-        );
-        const json = await query.json();
-        const data = json.routes[0];
-        const route = data.geometry.coordinates;
+        let result = await axios.get(`https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`);
+        let route = result.data.routes[0]["geometry"]["coordinates"];
         const geojson = {
             type: 'Feature',
             properties: {},
@@ -115,52 +110,53 @@ async function makeRoute() {
                 'circle-color': '#3887be'
             }
         });
-        const coords = [parseFloat(bathrooms[i]['longitude']), parseFloat(bathrooms[i]['latitude'])];
-        const end = {
-            type: 'FeatureCollection',
-            features: [
-                {
-                    type: 'Feature',
-                    properties: {},
-                    geometry: {
-                        type: 'Point',
-                        coordinates: coords
+        map.on('click', (event) => {
+            const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
+            const end = {
+                type: 'FeatureCollection',
+                features: [
+                    {
+                        type: 'Feature',
+                        properties: {},
+                        geometry: {
+                            type: 'Point',
+                            coordinates: coords
+                        }
                     }
-                }
-            ]
-        };
-        if (map.getLayer('end')) {
-            map.getSource('end').setData(end);
-        } else {
-            map.addLayer({
-                id: 'end',
-                type: 'circle',
-                source: {
-                    type: 'geojson',
-                    data: {
-                        type: 'FeatureCollection',
-                        features: [
-                            {
-                                type: 'Feature',
-                                properties: {},
-                                geometry: {
-                                    type: 'Point',
-                                    coordinates: coords
+                ]
+            };
+            if (map.getLayer('end')) {
+                map.getSource('end').setData(end);
+            } else {
+                map.addLayer({
+                    id: 'end',
+                    type: 'circle',
+                    source: {
+                        type: 'geojson',
+                        data: {
+                            type: 'FeatureCollection',
+                            features: [
+                                {
+                                    type: 'Feature',
+                                    properties: {},
+                                    geometry: {
+                                        type: 'Point',
+                                        coordinates: coords
+                                    }
                                 }
-                            }
-                        ]
+                            ]
+                        }
+                    },
+                    paint: {
+                        'circle-radius': 10,
+                        'circle-color': '#f30'
                     }
-                },
-                paint: {
-                    'circle-radius': 10,
-                    'circle-color': '#f30'
-                }
-            });
-        }
-        getRoute(coords);
+                });
+            }
+            getRoute(coords);
+        });
     });
 };
-
 let searchButton = document.getElementById("search-button");
 searchButton.addEventListener("click", async function (event) {
     event.preventDefault();
@@ -204,9 +200,11 @@ searchButton.addEventListener("click", async function (event) {
                 }).setLngLat([bathrooms[i]['longitude'], bathrooms[i]['latitude']])
                     .setPopup(popup) // sets a popup on this marker
                     .addTo(map);
+
             };
         };
-
+        // Listen for a click on the map
+        map.on('click', makeRoute);
     }).catch(function (error) {
         console.error(error);
     });
