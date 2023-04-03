97b7ec20fd2db21c4b8b0436cde2ba500727fba2
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-04-02 20:45:29 -0500

    added deault to username, and a filler bathroom

--- seed.sql
+++ seed.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
     id serial PRIMARY KEY,
     full_name text NOT NULL,
     email text NOT NULL UNIQUE,
-    username text NOT NULL UNIQUE,
+    username text NOT NULL UNIQUE DEFAULT 'Admin',
     password text NOT NULL
 );
 
@@ -25,3 +25,5 @@ CREATE TABLE bathrooms (
 INSERT INTO users (full_name, email, username, PASSWORD)
     VALUES ('Eddie Z', 'eddie@zeta.com', 'Eddie', '$2b$12$8ZyEYe2PtS9zQvnmKW03EOJH/k3gDJMVo14vSXWHH5tXwcJrxq4VO');
 
+INSERT INTO bathrooms (name, address, zip_code, longitude, latitude, website)
+    VALUES ('My House', '1036 N Dearborn St', '60610', '-87.63011829999999', '41.9016931', 'bjbproperties.com')
