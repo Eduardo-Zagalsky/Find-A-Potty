0eba4b6ca723a05a9aa8e3bd1c05e159a8d8df39
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-04-05 01:07:15 -0500

    fixing the tests to properly check them

--- test_app.py
+++ test_app.py
@@ -12,26 +12,29 @@ app.config['SQLALCHEMY_ECHO'] = False
 app.config['Testing'] = True
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
+db.drop_all()
+db.create_all()
+
 
 class Test_User(TestCase):
 
-    # def setUp(self):
-    #     # create table data
-    #     user = User(full_name="John Smith", email="johnsmith@email.com",
-    #                 username="JohnDoe", password="password")
-    #     potty = Potty(name="Grace Place", address="637 S Dearborn St", zip_code="60605", longitude="-87.6291",
-    #                   latitude="41.8735", website="http://gracechicago.org/grace-place/south-loop-campus-ministry/", added_by="admin")
-    #     # add data to a session and commit it to the table in database
-    #     db.session.add(user)
-    #     db.session.add(potty)
-    #     db.session.commit()
+    def setUp(self):
+        # create table data
+        user = User(full_name="John Smith", email="johnsmith@email.com",
+                    username="JohnDoe", password="password")
+        potty = Potty(name="Grace Place", address="637 S Dearborn St", zip_code="60605", longitude="-87.6291",
+                      latitude="41.8735", website="http://gracechicago.org/grace-place/south-loop-campus-ministry/")
+        # add data to a session and commit it to the table in database
+        db.session.add(user)
+        db.session.add(potty)
+        db.session.commit()
 
-    # def tearDown(self):
-    #     # undo all table data in session not yet commited
-    #     db.session.rollback()
-    #     # delete rows in table
-    #     User.query.delete()
-    #     Potty.query.delete()
+    def tearDown(self):
+        # undo all table data in session not yet commited
+        db.session.rollback()
+        # delete rows in table
+        User.query.delete()
+        Potty.query.delete()
 
     def test_get_info(self):
         # create table data
@@ -67,13 +70,14 @@ class Test_User(TestCase):
 
             self.assertEqual(resp.status_code, 200)
             # test signup for new user
-            users = User.query.all()
-            self.assertEqual(users, "user")
+            users = User.query.filter_by(email="janedoe@email.com")
+            self.assertEqual(users, {"full_name": "Jane Doe", "email": "janedoe@email.com",
+                                     "username": "JaneDoe", "password": "password"})
 
     def test_post_bathroom(self):
         with app.test_client() as client:
             resp = client.post("/profile", data={"name": "Millennium Park", "address": "201 E Randolph St", "zip_code": "60602", "longitude": "-87.6226",
-                                                 "latitude": "41.8826", "website": "https://www.chicago.gov/city/en/depts/dca/supp_info/millennium_park.html", "added_by": "admin"})
+                                                 "latitude": "41.8826", "website": "https://www.chicago.gov/city/en/depts/dca/supp_info/millennium_park.html"})
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
